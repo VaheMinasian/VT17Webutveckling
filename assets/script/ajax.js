a61e2384ff487991ea6cb87e08d569ec30ac64cb
Vahe Minasian <vahe_minasian@gmx.com>
2017-03-01 05:26:39 +0100

    first commit for voting

---
+++ assets/script/ajax.js
@@ -0,0 +1,36 @@
+"use strict"
+
+$(document).ready(function () {
+    $(".rating input").click(
+        function () {
+           var myRating = $(this).index() + 1;
+            rateRequest(myRating);
+        }
+    );
+});
+
+function rateRequest(myRating){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function (){
+		
+		if(this.readyState === 4 && this.status === 200){
+			displayResults();
+		}
+	}
+	xhttp.open("GET",
+		"https://edu.oscarb.se/sjk15/api/recipe/?api_key=91a5fd546b1086ec&recipe=%C3%84ppelpaj&rating=" + myRating,
+		true);
+	xhttp.send();
+}
+
+function displayResults () {
+    $.ajax({
+        method: "GET",
+        url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=7cd7a288a851a28b&recipe=mazarint%C3%A5rta",
+        success: function(data) {
+            var rating = data.rating.toFixed(1);
+            $("#average").text(rating);
+            $("#voters").text(data.votes);
+        },
+    });
+}
